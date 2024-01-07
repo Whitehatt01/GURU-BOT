bfecb27378dab24a75ee70312c53935813e1b021
Whitehatt01 <155889758+Whitehatt01@users.noreply.github.com>
2024-01-07 13:38:55 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['2349097234905', 'whitehatbot', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
